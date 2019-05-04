9c7214bacdcb58efba5bf89a9cc446817d2cab0b
Claudia melissa Gómez beltrán <claudiamelissagomezbeltran@dyn081191.shef.ac.uk>
2019-05-04 18:50:05 +0100

    i dont know

--- src/service/Mail.java
+++ src/service/Mail.java
@@ -12,12 +12,11 @@ import java.util.Properties;
 public class Mail {  
 public static void send(String to,String subject,String msg){  
   
-final String user="jbcyplatform@gmail.com";//change accordingly  
+final String user="jbcyplatform@gmail.com"; 
 final String pass="claudiagomez";  
-  
-//1st step) Get the session object    
+   
 Properties props = new Properties();  
-props.put("mail.smtp.host", "smtp.gmail.com");//change accordingly  
+props.put("mail.smtp.host", "smtp.gmail.com");
 props.put("mail.smtp.auth", "true");  
 props.put("mail.smtp.port", "587");
 props.put("mail.smtp.starttls.enable", "true");
@@ -28,7 +27,7 @@ Session session = Session.getDefaultInstance(props,
    return new PasswordAuthentication(user,pass);  
    }  
 });  
-//2nd step)compose message  
+ 
 try {  
  MimeMessage message = new MimeMessage(session);  
  message.setFrom(new InternetAddress(user));  
@@ -36,7 +35,7 @@ try {
  message.setSubject(subject);  
  message.setText(msg);  
    
- //3rd step)send message  
+  
  Transport.send(message);  
   
  System.out.println("Done");  
